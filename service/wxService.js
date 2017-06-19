44bcd559e8d2cb341d81203caea9d220d4b6ba32
zhang.yufeng <420866750@qq.com>
2017-06-19 13:39:23 +0800

    'dev'

--- service/wxService.js
+++ service/wxService.js
@@ -1,15 +1,43 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /*  API handlers - Wx                                                                        */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
 'use strict';
+
+const axios = require('axios');
+
+const APPID = 'wxd0124a9e51032874';
+const SECRET = 'a64c71252df6a6611e893c2ba53adf35';
+const TOKEN_URL = 'https://api.weixin.qq.com/sns/oauth2/access_token';
+const USER_URL = 'https://api.weixin.qq.com/sns/userinfo';
+const GRANT_TYPE = 'authorization_code';
 
 class WxService {
 
+    static get_access_token (c) {
+        new Promise((resolve, reject) =>{
+            axios.get(TOKEN_URL, {
+                appid: APPID,
+                secret: SECRET,
+                code: c,
+                grant_type: GRANT_TYPE
+            }).then(res => resolve(res));
+        }).then((res) => {
+            console.info(res.data);
+        });
+    }
+
     static async authWxLogin(ctx) {
-        let c = ctx.cookies.set('c');
+        let c = ctx.cookies.get('c');
         let path = ctx.query.state;
+        let code = ctx.query.path;
         let authUser = null;
+        if (c === undefined) {
+            let  access_token = WxService.get_access_token(code);
+
+        } else {
+
+        }
+
         ctx.body = 'auth';
     }
 
