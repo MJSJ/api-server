9192ad24535e3d48efc9cce624247ac50a9aede2
yursile <kuangyu02@meituan.com>
2018-02-28 16:00:14 +0800

    add other verify

--- app-api/routes/routes-wx.js
+++ app-api/routes/routes-wx.js
@@ -16,6 +16,12 @@ router.get('/MP_verify_oai1jPbiuXyaD710.txt', async function (ctx) {
     ctx.body = 'oai1jPbiuXyaD710';
 });
 
+
+//测试绑定其他公众号
+router.get('/MP_verify_LWMS70JBb0k89q4L.txt', async function (ctx) {
+    ctx.body = 'LWMS70JBb0k89q4L';
+});
+
 router.get('/wx/auth/activity', wxSerice.authWxLogin);
 
 router.get('/wx/jssdk', wxSerice.getJsSdkConf);
